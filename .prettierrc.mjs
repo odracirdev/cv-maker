d532ac866231b9dbda10c2aa4d73db31e1997ffe
Felix Icaza <fx.joliett17@gmail.com>
2024-07-11 22:33:23 -0600

    chore: update .prettierrc.mjs

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,28 +2,28 @@
 const config = {
   semi: false,
   singleQuote: true,
-  trailingComma: "none",
+  trailingComma: 'none',
   overrides: [
     {
-      files: "*.{yaml,yml}",
+      files: '*.{yaml,yml}',
       options: {
-        tabWidth: 3,
-      },
-    },
+        tabWidth: 3
+      }
+    }
   ],
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
   importOrder: [
-    "<BUILTIN_MODULES>",
-    "<THIRD_PARTY_MODULES>",
-    "^@data/(.*)$",
-    "^@layouts/(.*)$",
-    "^@pages/(.*)$",
-    "^@components/(.*)$",
-    "^@scripts/(.*)$",
-    "^@styles/(.*)$",
-    "^@/(.*)$",
-    "^.[./].*",
-  ],
-};
+    '<BUILTIN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '^@data/(.*)$',
+    '^@layouts/(.*)$',
+    '^@pages/(.*)$',
+    '^@components/(.*)$',
+    '^@scripts/(.*)$',
+    '^@styles/(.*)$',
+    '^@/(.*)$',
+    '^.[./].*'
+  ]
+}
 
-export default config;
+export default config
