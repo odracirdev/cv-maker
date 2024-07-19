48264ab588b0fa6dde0f705c6f02a549162aa67e
Facundodandrea <facundodandrea01@gmail.com>
2024-07-19 12:52:17 -0300

    fix: 🐛 routes frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
 import Chat from './components/Chat/Chat'
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <Chat />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </Router>
   )
 }
 
+export default App;
