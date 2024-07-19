881f4f870e1e7e2d9dd537bbcfbc1128a6112184
Facundodandrea <facundodandrea01@gmail.com>
2024-07-19 12:54:24 -0300

    feat: ✨ add speech recognition

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import useSpeechRecognition from '@/hooks/useSpeechaRecognitionHook';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const {
+    text,
+    startListening,
+    stopListening,
+    isListening,
+    hasRecognitionSupport,
+  } = useSpeechRecognition();
+
   return (
-    <>
-      <Link to='/chat' className='bg-black rounded-full text-white font-semibold p-4'>Go to chat</Link>
-    </>
-);
+    <div>
+      {hasRecognitionSupport ? (
+        <>
+          <div>
+            <button onClick={startListening}>Start Listening</button>
+            <button onClick={stopListening} disabled={!isListening}>Stop Listening</button>
+          </div>
+
+          {isListening ? (
+            <div>Navegador escuchando...</div>
+          ) : null}
+
+          <div>
+            <p>Texto: {text}</p>
+          </div>
+        </>
+      ) : (
+        <h1>Navegador no soportado</h1>
+      )}
+      <Link to='/chat' className='relative top-40 bg-black rounded-full text-white font-semibold p-4'>Go to chat</Link>
+    </div>
+  );
 };
 
 export default Home;
