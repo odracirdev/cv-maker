acedba9bce64a9986508497e6ff42c7fb6b61373
Felix Icaza <fx.joliett17@gmail.com>
2024-07-20 17:39:24 -0600

    chore: add Router and Route components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import Chat from './components/Chat/Chat'
+/* eslint-disable react/react-in-jsx-scope */
+
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <>
-      <Chat />
+      <Navbar />
+
+      <Routes>
+        <Route>
+          <Route index element={<div>Home page</div>} />
+          <Route path="/app" element={<div>App page</div>} />
+        </Route>
+      </Routes>
     </>
   )
 }
-
