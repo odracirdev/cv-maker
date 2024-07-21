2603bd1a76514991ab3dc1a8aa932eaee37f99c1
Felix Icaza <fx.joliett17@gmail.com>
2024-07-21 11:58:49 -0600

    chore: fix errors for build

--- frontend/src/components/ButtonLink/index.tsx
+++ frontend/src/components/ButtonLink/index.tsx
@@ -1,8 +1,14 @@
+;
 /* eslint-disable react/react-in-jsx-scope, react/prop-types */
 
 import { Link } from 'react-router-dom'
 
-export default function ButtonLink({ text, href }) {
+interface ButtonLinkProps {
+  text: string
+  href: string
+}
+
+const ButtonLink: React.FC<ButtonLinkProps> = ({ text, href }) => {
   return (
     <Link
       className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors inline-block"
@@ -12,3 +18,5 @@ export default function ButtonLink({ text, href }) {
     </Link>
   )
 }
+
+export default ButtonLink
