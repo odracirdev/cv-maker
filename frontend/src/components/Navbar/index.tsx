ef6865c9af548a8617098250ea9531ef87639897
Felix Icaza <fx.joliett17@gmail.com>
2024-07-20 18:03:31 -0600

    style: update Navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import { Link } from 'react-router-dom'
+import ButtonLink from '../ButtonLink'
 
 export default function Navbar() {
   return (
-    <nav className="container mx-auto px-4 flex justify-between py-11">
-      <Link
-        className="font-bold text-2xl hover:text-blue-500 transition-colors"
-        to="/"
-      >
-        CV Maker
-      </Link>
-      <Link
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors"
-        to="/app"
-      >
-        Inicia ahora
-      </Link>
+    <nav className="sticky top-0 border-b border-neutral-800 py-6">
+      <div className="container mx-auto px-4 flex justify-between">
+        <Link
+          className="font-bold text-2xl hover:text-blue-500 transition-colors"
+          to="/"
+        >
+          CV Maker
+        </Link>
+        <ButtonLink href="/app" text="Inicia Ahora" />
+      </div>
     </nav>
   )
 }
