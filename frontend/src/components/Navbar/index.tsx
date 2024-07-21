4c8092b4ba0f919f50fb2abdb2c1e6f771c4149e
Felix Icaza <fx.joliett17@gmail.com>
2024-07-20 18:08:22 -0600

    style: update styles in Navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import ButtonLink from '../ButtonLink'
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 border-b border-neutral-800 py-6">
+    <nav className="sticky top-0 border-b border-neutral-800 py-6 bg-neutral-900/90 backdrop-blur-sm">
       <div className="container mx-auto px-4 flex justify-between">
         <Link
           className="font-bold text-2xl hover:text-blue-500 transition-colors"
