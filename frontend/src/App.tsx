f0315a7255c322d993479538cf77f65397fbe493
Facundodandrea <facundodandrea01@gmail.com>
2024-07-20 22:56:10 -0300

    fix: :bug: arreglo rutas front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import Home from './pages/home/Home'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Chat from './components/Chat/Chat'
+import Home from './pages/home/Home'
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/chat' element={<Chat />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
   )
 }
 
-export default App;
+export default App
