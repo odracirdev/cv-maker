4a640cc5011ebf94312c6cb2755f96d6bd6ef047
Felix Icaza <fx.joliett17@gmail.com>
2024-07-22 19:48:49 -0600

    feat: add aurora component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import { Route, Routes } from 'react-router-dom'
+import { AuroraBackground } from '@/components/ui/aurora-background'
 import Chat from './components/Chat/Chat'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
@@ -9,6 +10,10 @@ import Home from './pages/Home'
 export default function App() {
   return (
     <>
+      <AuroraBackground className="absolute w-full h-screen top-0 left-0 opacity-80">
+        <div></div>
+      </AuroraBackground>
+
       <Navbar />
 
       <Routes>
