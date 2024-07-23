6b386982dad7f9a800366ff46c7ed64e4a3d8a7e
Felix Icaza <fx.joliett17@gmail.com>
2024-07-22 21:04:16 -0600

    style: feature section style

---
+++ frontend/src/pages/Home/components/Features/index.tsx
@@ -0,0 +1,124 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function Features() {
+  return (
+    <article className="py-16 space-y-[72px]">
+      <div className="space-y-6 text-center">
+        <h2 className="text-4xl font-bold bg-gradient-to-r from-white to-[#949494] bg-clip-text text-transparent">
+          Lorem ipsum dolor sit amet
+        </h2>
+        <p className="text-[#a3a3a3] max-w-[704px] mx-auto">
+          Lorem ipsum dolor sit amet consectetur. Massa semper enim accumsan
+          tristique sit ornare nisi. Amet nisl et auctor nunc. Amet urna quis id
+          at sed sed mattis.
+        </p>
+      </div>
+      <section className="grid grid-cols-2 gap-x-[72px] items">
+        <div className="px-4 py-6 flex gap-2.5">
+          <span>
+            <svg fill="none" viewBox="0 0 24 24" width={24}>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              />
+            </svg>
+          </span>
+          <p>
+            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
+            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
+            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
+            eros pharetra.
+          </p>
+        </div>
+        <div className="px-4 py-6 flex gap-2.5">
+          <span>
+            <svg fill="none" viewBox="0 0 24 24" width={24}>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              />
+            </svg>
+          </span>
+          <p>
+            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
+            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
+            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
+            eros pharetra.
+          </p>
+        </div>
+        <div className="px-4 py-6 flex gap-2.5">
+          <span>
+            <svg fill="none" viewBox="0 0 24 24" width={24}>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              />
+            </svg>
+          </span>
+          <p>
+            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
+            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
+            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
+            eros pharetra.
+          </p>
+        </div>
+        <div className="px-4 py-6 flex gap-2.5">
+          <span>
+            <svg fill="none" viewBox="0 0 24 24" width={24}>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              />
+            </svg>
+          </span>
+          <p>
+            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
+            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
+            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
+            eros pharetra.
+          </p>
+        </div>
+      </section>
+    </article>
+  )
+}
