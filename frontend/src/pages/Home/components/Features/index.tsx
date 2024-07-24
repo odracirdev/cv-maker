74ce40bd5f0863c85de3f8ca611ea514bfdbfdb1
Felix Icaza <fx.joliett17@gmail.com>
2024-07-23 20:39:09 -0600

    style: update Features component

--- frontend/src/pages/Home/components/Features/index.tsx
+++ frontend/src/pages/Home/components/Features/index.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import './index.css'
+
 export default function Features() {
   return (
-    <article className="py-16 space-y-[72px]">
+    <article className="pt-12 pb-44 space-y-[72px]">
       <div className="space-y-6 text-center">
-        <h2 className="text-4xl font-bold bg-gradient-to-r from-white to-[#949494] bg-clip-text text-transparent">
+        <h2 className="text-[2.5rem] font-bold bg-gradient-to-r from-white to-[#949494] bg-clip-text text-transparent">
           Lorem ipsum dolor sit amet
         </h2>
-        <p className="text-[#a3a3a3] max-w-[704px] mx-auto">
+        <p className="text-neutral-400 max-w-[704px] mx-auto">
           Lorem ipsum dolor sit amet consectetur. Massa semper enim accumsan
           tristique sit ornare nisi. Amet nisl et auctor nunc. Amet urna quis id
           at sed sed mattis.
         </p>
       </div>
-      <section className="grid grid-cols-2 gap-x-[72px]">
-        <div className="px-4 py-6 flex gap-2.5">
+      <section className="grid grid-cols-2 gap-x-[72px] relative items-container">
+        <div className="px-4 py-6 flex gap-2.5 border-t border-b border-neutral-800">
           <span>
             <svg fill="none" viewBox="0 0 24 24" width={24}>
               <path
@@ -33,14 +35,14 @@ export default function Features() {
               />
             </svg>
           </span>
-          <p>
-            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
-            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
-            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
-            eros pharetra.
+          <p className="text-neutral-400">
+            <strong className="text-white">Plantillas</strong>: Lorem ipsum
+            dolor sit amet consectetur. Sodales platea ut vestibulum lacus nisl
+            ac habitant amet at. Facilisis morbi id consectetur consequat. A
+            arcu imperdiet blandit eros pharetra.
           </p>
         </div>
-        <div className="px-4 py-6 flex gap-2.5">
+        <div className="px-4 py-6 flex gap-2.5 border-t border-b border-neutral-800">
           <span>
             <svg fill="none" viewBox="0 0 24 24" width={24}>
               <path
@@ -59,14 +61,14 @@ export default function Features() {
               />
             </svg>
           </span>
-          <p>
-            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
-            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
-            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
-            eros pharetra.
+          <p className="text-neutral-400">
+            <strong className="text-white">Plantillas</strong>: Lorem ipsum
+            dolor sit amet consectetur. Sodales platea ut vestibulum lacus nisl
+            ac habitant amet at. Facilisis morbi id consectetur consequat. A
+            arcu imperdiet blandit eros pharetra.
           </p>
         </div>
-        <div className="px-4 py-6 flex gap-2.5">
+        <div className="px-4 py-6 flex gap-2.5 border-b border-neutral-800">
           <span>
             <svg fill="none" viewBox="0 0 24 24" width={24}>
               <path
@@ -85,14 +87,14 @@ export default function Features() {
               />
             </svg>
           </span>
-          <p>
-            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
-            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
-            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
-            eros pharetra.
+          <p className="text-neutral-400">
+            <strong className="text-white">Plantillas</strong>: Lorem ipsum
+            dolor sit amet consectetur. Sodales platea ut vestibulum lacus nisl
+            ac habitant amet at. Facilisis morbi id consectetur consequat. A
+            arcu imperdiet blandit eros pharetra.
           </p>
         </div>
-        <div className="px-4 py-6 flex gap-2.5">
+        <div className="px-4 py-6 flex gap-2.5 border-b border-neutral-800">
           <span>
             <svg fill="none" viewBox="0 0 24 24" width={24}>
               <path
@@ -111,11 +113,11 @@ export default function Features() {
               />
             </svg>
           </span>
-          <p>
-            <strong>Plantillas</strong>: Lorem ipsum dolor sit amet consectetur.
-            Sodales platea ut vestibulum lacus nisl ac habitant amet at.
-            Facilisis morbi id consectetur consequat. A arcu imperdiet blandit
-            eros pharetra.
+          <p className="text-neutral-400">
+            <strong className="text-white">Plantillas</strong>: Lorem ipsum
+            dolor sit amet consectetur. Sodales platea ut vestibulum lacus nisl
+            ac habitant amet at. Facilisis morbi id consectetur consequat. A
+            arcu imperdiet blandit eros pharetra.
           </p>
         </div>
       </section>
