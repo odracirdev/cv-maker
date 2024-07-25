af652895ffc6e14b53e208ff4eb4568445ced4fa
Felix Icaza <fx.joliett17@gmail.com>
2024-07-24 20:18:20 -0600

    style: init chat section

--- frontend/src/pages/Chat/index.tsx
+++ frontend/src/pages/Chat/index.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
+
 import { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
 import { AuroraBackground } from '@/components/ui/aurora-background'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
-import './index.css'
 import ChatBox from '../../components/ChatBox'
 import ChatBubble from '../../components/ChatBubble'
 import useChat from '../../contexts/chat'
 import useSpeechRecognition from '../../contexts/speech-recognition'
+import './index.css'
 
 export default function Chat() {
   const initSpeechRecognition = useSpeechRecognition((state) => state.init)
@@ -31,38 +31,30 @@ export default function Chat() {
       <AuroraBackground
         className="absolute w-full h-screen top-0 left-0 opacity-80 pointer-events-none"
         pause={true}
-      >
-        <div />
-      </AuroraBackground>
-      <div className="h-screen w-screen overflow-hidden">
-        <section className="w-1/2 max-h-screen bg-zinc-950 flex flex-col justify-between overflow-hidden">
+      />
+
+      <div className="h-screen w-screen overflow-hidden grid grid-cols-2 divide-x divide-neutral-700">
+        <section>
           <div>
-            <div className="space-y-1.5 p-6 flex flex-row items-center justify-between">
-              <div className="flex items-center space-x-4">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div>
-                  <p className="text-sm font-medium leading-none text-white">
-                    Elena Banana
-                  </p>
-                  <p className="text-sm text-gray-400">m@example.com</p>
-                </div>
-              </div>
-              <Button
-                variant="outline"
-                size="icon"
-                className="text-white flex items-center justify-center whitespace-nowrap rounded-full h-10 w-10"
+            <section className="py-7 px-9 border-b border-neutral-700">
+              <Link
+                to="/"
+                className="text-neutral-400 flex items-center gap-4 hover:text-white transition-colors"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="m12 15.4l-6-6L7.4 8l4.6 4.6L16.6 8L18 9.4z"
-                  />
-                </svg>
-              </Button>
-            </div>
+                <span>
+                  <svg width={32} height={32} fill="none">
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M25.3 16H6.7M16 25.3 6.7 16 16 6.7"
+                    />
+                  </svg>
+                </span>
+                <p>Salir</p>
+              </Link>
+            </section>
 
             <div className="p-6 pt-0 chat-container">
               <div className="space-y-4 overflow-x-hidden" ref={chatRef}>
@@ -80,6 +72,10 @@ export default function Chat() {
           <div className="flex items-center px-6 py-4">
             <ChatBox />
           </div>
+        </section>
+
+        <section>
+          <p>CV</p>
         </section>
       </div>
     </>
