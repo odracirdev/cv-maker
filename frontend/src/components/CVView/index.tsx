31007261c40f032304f1e7f6ee78c70722f34993
Felix Icaza <fx.joliett17@gmail.com>
2024-07-30 17:09:53 -0600

    feat: init CV view

---
+++ frontend/src/components/CVView/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import About from './About'
+
+export default function CvView() {
+  return (
+    <div className="bg-stone-900 border border-neutral-800 p-6 rounded-[8px]">
+      <About />
+    </div>
+  )
+}
