de3b8958d5857a69e7b548b1b77f0e217fddbcb9
Felix Icaza <fx.joliett17@gmail.com>
2024-07-30 17:30:36 -0600

    chore: add Experience section

---
+++ frontend/src/components/CVView/Experience/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import Job from './Job'
+
+export default function Experience() {
+  return (
+    <section className="space-y-3">
+      <p className="font-semibold text-xl border-b border-white pb-3">
+        Experiencia laboral
+      </p>
+      <Job />
+      <Job />
+    </section>
+  )
+}
--- frontend/src/components/CVView/index.tsx
+++ frontend/src/components/CVView/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import About from './About'
+import Experience from './Experience'
 
 export default function CvView() {
   return (
-    <div className="bg-stone-900 border border-neutral-800 p-6 rounded-[8px]">
+    <div className="bg-stone-900 border border-neutral-800 p-6 rounded-[8px] space-y-12">
       <About />
+      <Experience />
     </div>
   )
 }
